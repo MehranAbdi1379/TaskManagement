e49b9c0d3b7193d15c8dfe864dd52700d83d45b9
Mehran Abdi <mehranabdi13793@gmail.com>
2025-02-26 22:41:25 +0330

    Add Fluent validation for backend

---
+++ TaskManagement.Shared/Validators/UpdateTaskDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Service.DTOs.Task;
+
+namespace TaskManagement.Service.Validators
+{
+    public class UpdateTaskDtoValidator: AbstractValidator<UpdateTaskDto>
+    {
+        public UpdateTaskDtoValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(100).WithMessage("Title cannot exceed 100 characters.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(300).WithMessage("Description cannot exceed 300 characters.");
+
+            RuleFor(x => (int)x.Status)
+                .InclusiveBetween(0, 3).WithMessage("Status code should be from 0 to 3");
+        }
+    }
+}
---
+++ TaskManagement.Shared/Validators/UpdateTaskStatusDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Service.DTOs.Task;
+
+namespace TaskManagement.Service.Validators
+{
+    public class UpdateTaskStatusDtoValidator: AbstractValidator<UpdateTaskStatusDto>
+    {
+        public UpdateTaskStatusDtoValidator()
+        {
+            RuleFor(x => (int)x.Status)
+               .InclusiveBetween(0, 3).WithMessage("Status code should be from 0 to 3");
+        }
+    }
+}
