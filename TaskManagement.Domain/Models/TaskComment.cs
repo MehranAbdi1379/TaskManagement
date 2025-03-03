0f9cfc0f1caa38e66e1b9880ba497cff65aebae5
Mehran Abdi <mehranabdi13793@gmail.com>
2025-03-04 01:11:25 +0330

    Add task comment entity

---
+++ TaskManagement.Domain/Models/TaskComment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagement.Domain.Models
+{
+    public class TaskComment: BaseEntity
+    {
+        public int TaskId { get; set; }
+        public int UserId { get; set; }
+        public string Text { get; set; }
+    }
+}
