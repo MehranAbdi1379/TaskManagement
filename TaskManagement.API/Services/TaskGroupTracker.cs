63c1caf203eb1215b2a2ad51fde2567762640f12
Mehran Abdi <mehranabdi13793@gmail.com>
2025-04-29 18:34:28 +0330

    Add TaskGroupTracker to backend

---
+++ TaskManagement.API/Services/TaskGroupTracker.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskManagement.API.Services;
+
+public class TaskGroupTracker
+{
+    public List<(int userId,int taskId)> TaskUserGroups { get; private set; } = [];
+
+    public void AddTaskUserGroup(int userId, int taskId)
+    {
+        if(TaskUserGroups.Count == 0 || !TaskUserGroups.Any(x => x.userId == userId && x.taskId == taskId)) TaskUserGroups.Add(new (userId,taskId));
+    }
+
+    public void RemoveTaskUserGroup(int userId, int taskId)
+    {
+        if(TaskUserGroups.Any(x => x.userId == userId && x.taskId == taskId)) TaskUserGroups.Remove((userId, taskId));
+    }
+}
