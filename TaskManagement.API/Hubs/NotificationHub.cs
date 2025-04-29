2d91ed327b285c9a4df4cf3234cb524e159d2776
Mehran Abdi <mehranabdi13793@gmail.com>
2025-04-29 18:36:54 +0330

    Add singleton group management to Notification Hub

--- TaskManagement.API/Hubs/NotificationHub.cs
+++ TaskManagement.API/Hubs/NotificationHub.cs
@@ -1,20 +1,33 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.SignalR;
+using TaskManagement.API.Services;
 using TaskManagement.Shared.DTOs.Notification;
 using TaskManagement.Shared.DTOs.TaskComment;
+using TaskManagement.Shared.ServiceInterfaces;
 
 namespace TaskManagement.API.Hubs
 {
     public class NotificationHub : Hub
     {
+        private readonly TaskGroupTracker _taskGroupTracker;
+        private readonly IUserContext _userContext;
+
+        public NotificationHub(TaskGroupTracker taskGroupTracker, IUserContext userContext)
+        {
+            _taskGroupTracker = taskGroupTracker;
+            _userContext = userContext;
+        }
         // Join a task group (users viewing a specific task will join)
         public async Task JoinTaskGroup(string groupName)
         {
+            _taskGroupTracker.AddTaskUserGroup(_userContext.UserId, int.Parse(Regex.Match(groupName, @"\d+").Value));
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
         }
 
         // Leave a task group (when a user navigates away)
         public async Task LeaveTaskGroup(string groupName)
         {
+            _taskGroupTracker.RemoveTaskUserGroup(_userContext.UserId, int.Parse(Regex.Match(groupName, @"\d+").Value));
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
         }
     }
