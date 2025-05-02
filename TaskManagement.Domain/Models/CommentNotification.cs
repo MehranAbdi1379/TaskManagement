250bb50f2df96b20e404401c88014e2ba901d7b9
Mehran Abdi <mehranabdi13793@gmail.com>
2025-05-02 20:18:56 +0330

    Create comment notification to track notifications created for new comments

---
+++ TaskManagement.Domain/Models/CommentNotification.cs
@@ -0,0 +1,28 @@
+﻿namespace TaskManagement.Domain.Models;
+
+public class CommentNotification: BaseEntity
+{
+    public int NotificationId { get; private set; }
+    public int CommentId { get; private set; }
+
+    public CommentNotification()
+    {
+            
+    }
+
+    public CommentNotification(int notificationId, int commentId)
+    {
+        SetNotificationId(notificationId);
+        SetCommentId(commentId);
+    }
+
+    public void SetNotificationId(int userId)
+    {
+        NotificationId = userId;
+    }
+
+    public void SetCommentId(int taskId)
+    {   
+        CommentId = taskId;
+    }
+}
