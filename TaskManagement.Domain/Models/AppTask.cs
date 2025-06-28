a88183893f98db44ec9491523ea3e0a1973fdcfd
Mehran Abdi <mehranabdi13793@gmail.com>
2025-06-28 17:16:49 +0330

    change AppTask.cs to DDD

--- TaskManagement.Domain/Models/AppTask.cs
+++ TaskManagement.Domain/Models/AppTask.cs
@@ -1,52 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TaskManagement.Domain.Enums;
+﻿using TaskManagement.Domain.Enums;
 using TaskManagement.Domain.Exceptions;
 
-namespace TaskManagement.Domain.Models
+namespace TaskManagement.Domain.Models;
+
+public class AppTask : BaseEntity
 {
-    public class AppTask: BaseEntity
+    public AppTask()
     {
-        public string Title { get; private set; }
-        public string Description { get; private set; }
-        public Status Status { get; private set; }
-        public int OwnerId { get; private set; }
+    }
 
-        public AppTask()
-        {
+    public AppTask(string title, string description, Status taskStatus)
+    {
+        SetTitle(title);
+        SetDescription(description);
+        SetStatus(taskStatus);
+    }
 
-        }
+    public string Title { get; private set; }
+    public string Description { get; private set; }
+    public Status Status { get; private set; }
+    public int OwnerId { get; private set; }
 
-        public AppTask(string title, string description, Status taskStatus)
-        {
-            SetTitle(title);
-            SetDescription(description);
-            SetStatus(taskStatus);
-        }
 
-        public void SetTitle(string title)
-        {
-            if (string.IsNullOrEmpty(title)) throw new DomainException("Title of the task can not be empty.");
-            Title = title;
-        }
+    public void SetTitle(string title)
+    {
+        ValidateTitle(title);
+        Title = title;
+    }
 
-        public void SetDescription(string description)
-        {
-            Description = description;
-        }
+    private void ValidateTitle(string title)
+    {
+        if (string.IsNullOrEmpty(title)) throw new DomainException("Title of the task can not be empty.");
+    }
 
-        public void SetStatus(Status taskStatus)
-        {
-            if ((int)taskStatus < 0 || (int)taskStatus > 3) throw new DomainException("Task Status Code should be between 0 and 3");
-            Status = taskStatus;
-        }
+    public void SetDescription(string description)
+    {
+        Description = description;
+    }
 
-        public void SetOwnerId(int userId)
-        {
-            OwnerId = userId;
-        }
+    public void SetStatus(Status taskStatus)
+    {
+        ValidateStatus(taskStatus);
+        Status = taskStatus;
+    }
+
+    private void ValidateStatus(Status taskStatus)
+    {
+        if (!Enum.IsDefined(typeof(TaskStatus), taskStatus))
+            throw new DomainException($"Invalid Task Status Code with code {taskStatus} for Task Id {Id}");
+    }
+
+    public void SetOwnerId(int userId)
+    {
+        OwnerId = userId;
     }
 }
