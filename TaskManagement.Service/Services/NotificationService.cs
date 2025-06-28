f3d9bbc20cb8bcbac658f2bfcd3fd0b85ae7171f
Mehran Abdi <mehranabdi13793@gmail.com>
2025-06-28 17:19:09 +0330

    Modify NotificationService.cs to DDD (delete dependencies to pagination and DTOs)

--- TaskManagement.Service/Services/NotificationService.cs
+++ TaskManagement.Service/Services/NotificationService.cs
@@ -1,55 +1,78 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
 using TaskManagement.Domain.Enums;
+using TaskManagement.Domain.Interfaces;
 using TaskManagement.Domain.Models;
-using TaskManagement.Repository.Repositories;
-using TaskManagement.Service.DTOs;
+using TaskManagement.Shared.DTOs;
 using TaskManagement.Shared.DTOs.Notification;
 using TaskManagement.Shared.ServiceInterfaces;
 
-namespace TaskManagement.Service.Services
+namespace TaskManagement.Service.Services;
+
+public class NotificationService : INotificationService
 {
-    public class NotificationService : INotificationService
+    private readonly INotificationRepository baseRepository;
+    private readonly IMapper mapper;
+    private readonly IUserContext userContext;
+
+
+    public NotificationService(INotificationRepository baseRepository, IUserContext userContext, IMapper mapper)
     {
-        private readonly INotificationRepository baseRepository;
-        private readonly IUserContext userContext;
+        this.baseRepository = baseRepository;
+        this.userContext = userContext;
+        this.mapper = mapper;
+    }
 
+    public async Task<BaseNotification> CreateNotification(int userId, string title, string content,
+        NotificationType notificationType)
+    {
+        var notification = new BaseNotification(userId, title, content, notificationType);
 
-        public NotificationService(INotificationRepository baseRepository, IUserContext userContext)
+        return await baseRepository.AddAsync(notification);
+    }
+
+    public async Task<PagedResult<NotificationResponseDto>> GetUserNotificationsAsync(
+        NotificationQueryParameters parameters)
+    {
+        var (notifications, totalCount) =
+            await baseRepository.GetActiveNotificationsByUserId(parameters.PageNumber, parameters.PageSize,
+                parameters.SortOrder);
+
+        var mappedResult = mapper.Map<List<NotificationResponseDto>>(notifications);
+
+        return new PagedResult<NotificationResponseDto>
         {
-            this.baseRepository = baseRepository;
-            this.userContext = userContext;
-        }
+            Items = mappedResult,
+            TotalCount = totalCount,
+            PageNumber = parameters.PageNumber,
+            PageSize = parameters.PageSize
+        };
+    }
 
-        public async Task<BaseNotification> CreateNotification(int userId, string title, string content, NotificationType notificationType)
+    public async Task<PagedResult<NotificationResponseDto>> GetNotificationHistoryAsync(
+        NotificationQueryParameters parameters)
+    {
+        var (notifications, totalCount) =
+            await baseRepository.GetNotificationHistoryByUserId(parameters.PageSize, parameters.PageNumber,
+                parameters.SortOrder);
+
+        var mappedResult = mapper.Map<List<NotificationResponseDto>>(notifications);
+
+        return new PagedResult<NotificationResponseDto>
         {
-            var notification = new BaseNotification(userId, title, content, notificationType);
+            Items = mappedResult,
+            TotalCount = totalCount,
+            PageNumber = parameters.PageNumber,
+            PageSize = parameters.PageSize
+        };
+    }
 
-            return await baseRepository.AddAsync(notification);
-        }
+    public async Task UpdateNotificationIsReadAsync(int notificationId)
+    {
+        var notification = await baseRepository.GetByIdAsync(notificationId);
+        if (notification.UserId != userContext.UserId) throw new Exception("Notification does not belong to the user.");
 
-        public async Task<PagedResult<NotificationResponseDto>> GetUserNotificationsAsync(NotificationQueryParameters parameters)
-        {
-            return await baseRepository.GetActiveNotificationsByUserId(parameters);
-        }
+        notification.ReadNotification();
 
-        public async Task<PagedResult<NotificationResponseDto>> GetNotificationHistoryAsync(NotificationQueryParameters parameters)
-        {
-            return await baseRepository.GetNotificationHistoryByUserId(parameters);
-        }
-
-        public async Task UpdateNotificationIsReadAsync(int notificationId)
-        {
-            var notification = await baseRepository.GetByIdAsync(notificationId);
-            if (notification.UserId != userContext.UserId) throw new Exception("Notification does not belong to the user.");
-
-            notification.SetIsRead(true);
-
-            await baseRepository.UpdateAsync(notification);
-        }
+        await baseRepository.UpdateAsync(notification);
     }
 }
