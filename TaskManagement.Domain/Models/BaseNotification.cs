f984a099620ca78867a93981fa8b42ba4ade04b1
Mehran Abdi <mehranabdi13793@gmail.com>
2025-06-28 17:17:36 +0330

    change BaseNotification.cs to DDD and add validations

--- TaskManagement.Domain/Models/BaseNotification.cs
+++ TaskManagement.Domain/Models/BaseNotification.cs
@@ -1,57 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TaskManagement.Domain.Enums;
+﻿using TaskManagement.Domain.Enums;
+using TaskManagement.Domain.Exceptions;
 
-namespace TaskManagement.Domain.Models
+namespace TaskManagement.Domain.Models;
+
+public class BaseNotification : BaseEntity
 {
-    public class BaseNotification: BaseEntity
+    public BaseNotification()
     {
-        public int UserId { get; private set; }
-        public string Title { get; private set; }
-        public string Content { get; private set; }
-        public NotificationType Type { get; private set; }
-        public bool IsRead { get; private set; }
+    }
 
-        public BaseNotification()
-        {
-            
-        }
+    public BaseNotification(int userId, string title, string content, NotificationType type)
+    {
+        SetUserId(userId);
+        SetTitle(title);
+        SetContent(content);
+        SetType(type);
+    }
 
-        public BaseNotification(int userId, string title, string content, NotificationType type)
-        {
-            SetUserId(userId);
-            SetTitle(title);
-            SetContent(content);
-            SetType(type);
-            IsRead = false;
-        }
+    public int UserId { get; private set; }
+    public string Title { get; private set; }
+    public string Content { get; private set; }
+    public NotificationType Type { get; private set; }
+    public bool IsRead { get; private set; }
 
-        public void SetUserId(int userId)
-        {
-            UserId = userId;
-        }
+    public void SetUserId(int userId)
+    {
+        UserId = userId;
+    }
 
-        public void SetTitle(string title)
-        {
-            Title = title;
-        }
+    public void SetTitle(string title)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new DomainException("Title cannot be null or empty.");
+        Title = title;
+    }
 
-        public void SetContent(string content)
-        {
-            Content = content;
-        }
+    public void SetContent(string content)
+    {
+        if (string.IsNullOrWhiteSpace(content))
+            throw new DomainException("Content cannot be null or empty.");
+        Content = content;
+    }
 
-        public void SetType(NotificationType notificationType)
-        {
-            Type = notificationType;
-        }
+    public void SetType(NotificationType notificationType)
+    {
+        if (!Enum.IsDefined(typeof(NotificationType), notificationType))
+            throw new DomainException("Invalid notification type.");
+        Type = notificationType;
+    }
 
-        public void SetIsRead(bool isRead)
-        {
-            IsRead = isRead;
-        }
+    public void ReadNotification()
+    {
+        IsRead = true;
     }
 }
