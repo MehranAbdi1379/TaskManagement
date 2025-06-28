f1396a0bbf33a06963562215ae6a686ca6ce58d8
Mehran Abdi <mehranabdi13793@gmail.com>
2025-06-28 17:17:19 +0330

    change BaseEntity.cs to DDD

--- TaskManagement.API/Controllers/AuthController.cs
+++ TaskManagement.API/Controllers/AuthController.cs
@@ -1,125 +1,117 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
+﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 using TaskManagement.Domain.Models;
-using TaskManagement.Service.Services;
 using TaskManagement.Shared.DTOs.Authentication;
+using TaskManagement.Shared.ServiceInterfaces;
 
-namespace TaskManagement.API.Controllers
+namespace TaskManagement.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthController : ControllerBase
+    private readonly IJwtService _jwtService;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AuthController(UserManager<ApplicationUser> userManager, IJwtService jwtService)
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly IJwtService _jwtService;
+        _userManager = userManager;
+        _jwtService = jwtService;
+    }
 
-        public AuthController(UserManager<ApplicationUser> userManager, IJwtService jwtService)
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterDto model)
+    {
+        var user = new ApplicationUser { UserName = model.Username, Email = model.Username };
+        user.SetFirstName(model.FirstName);
+        user.SetLastName(model.LastName);
+        var result = await _userManager.CreateAsync(user, model.Password);
+
+        if (!result.Succeeded)
+            return BadRequest(result.Errors);
+
+        var token = await _jwtService.GenerateToken(user);
+
+        var cookieOptions = new CookieOptions
         {
-            _userManager = userManager;
-            _jwtService = jwtService;
-        }
+            HttpOnly = true, // Prevents access from JavaScript
+            Secure = true, // Ensures it's only sent over HTTPS
+            SameSite = SameSiteMode.None, // Prevents CSRF
+            Expires = DateTime.UtcNow.AddHours(5)
+        };
 
-        [HttpPost("register")]
-        public async Task<IActionResult> Register([FromBody] RegisterDto model)
+        Response.Cookies.Append("jwt", token, cookieOptions);
+
+        return Ok("User created successfully!");
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginDto model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+        if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+            return Unauthorized("Invalid credentials");
+
+        var token = await _jwtService.GenerateToken(user);
+
+        var cookieOptions = new CookieOptions
         {
-            var user = new ApplicationUser { UserName = model.Username, Email = model.Username,FirstName = model.FirstName, LastName = model.LastName};
-            var result = await _userManager.CreateAsync(user, model.Password);
+            HttpOnly = true, // Prevents access from JavaScript
+            Secure = true, // Ensures it's only sent over HTTPS
+            SameSite = SameSiteMode.None, // Prevents CSRF
+            Expires = DateTime.UtcNow.AddHours(5)
+        };
 
-            if (!result.Succeeded)
-                return BadRequest(result.Errors);
+        Response.Cookies.Append("jwt", token, cookieOptions);
 
-            var token = await _jwtService.GenerateToken(user);
-                
-            var cookieOptions = new CookieOptions
-            {
-                HttpOnly = true, // Prevents access from JavaScript
-                Secure = true,   // Ensures it's only sent over HTTPS
-                SameSite = SameSiteMode.None, // Prevents CSRF
-                Expires = DateTime.UtcNow.AddHours(5)
-            };
+        return Ok("Login successful");
+    }
 
-            Response.Cookies.Append("jwt", token, cookieOptions);
-
-            return Ok("User created successfully!");
-        }
-
-        [HttpPost("login")]
-        public async Task<IActionResult> Login([FromBody] LoginDto model)
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        Response.Cookies.Append("jwt", "", new CookieOptions
         {
-            var user = await _userManager.FindByNameAsync(model.Username);
-            if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
-                return Unauthorized("Invalid credentials");
+            Expires = DateTime.UtcNow.AddDays(-1),
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.None
+        });
+        return Ok(new { message = "Logged out successfully" });
+    }
 
-            var token = await _jwtService.GenerateToken(user);
+    [HttpGet("status")]
+    public IActionResult GetAuthStatus()
+    {
+        // Check if the user is authenticated
+        if (User.Identity.IsAuthenticated)
+            // Return a response indicating that the user is authenticated
+            return Ok(new { isAuthenticated = true });
 
-            var cookieOptions = new CookieOptions
-            {
-                HttpOnly = true, // Prevents access from JavaScript
-                Secure = true,   // Ensures it's only sent over HTTPS
-                SameSite = SameSiteMode.None, // Prevents CSRF
-                Expires = DateTime.UtcNow.AddHours(5)
-            };
+        // Return a response indicating that the user is not authenticated
+        return Ok(new { isAuthenticated = false });
+    }
 
-            Response.Cookies.Append("jwt", token, cookieOptions);
+    [HttpGet("get-user-id")]
+    public IActionResult GetUserId()
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // Standard Claim "nameid"
+        if (userId == null) return Unauthorized("User ID not found in token.");
 
-            return Ok("Login successful");
-        }
-
-        [HttpPost("logout")]
-        public IActionResult Logout()
-        {
-            Response.Cookies.Append("jwt", "", new CookieOptions
-            {
-                Expires = DateTime.UtcNow.AddDays(-1), 
-                HttpOnly = true, 
-                Secure = true,    
-                SameSite = SameSiteMode.None, 
-            });
-            return Ok(new { message = "Logged out successfully" });
-        }
-
-        [HttpGet("status")]
-        public IActionResult GetAuthStatus()
-        {
-            // Check if the user is authenticated
-            if (User.Identity.IsAuthenticated)
-            {
-                // Return a response indicating that the user is authenticated
-                return Ok(new { isAuthenticated = true });
-            }
-            else
-            {
-                // Return a response indicating that the user is not authenticated
-                return Ok(new { isAuthenticated = false });
-            }
-        }
-
-        [HttpGet("get-user-id")]
-        public IActionResult GetUserId()
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // Standard Claim "nameid"
-            if (userId == null)
-            {
-                return Unauthorized("User ID not found in token.");
-            }
-
-            return Ok(new { UserId = userId });
-        }
+        return Ok(new { UserId = userId });
+    }
 
 
-        [HttpPost("generateToken")]
-        public async Task<IActionResult> GenerateToken([FromBody] LoginDto model)
-        {
-            var user = await _userManager.FindByNameAsync(model.Username);
-            if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
-                return Unauthorized("Invalid credentials");
+    [HttpPost("generateToken")]
+    public async Task<IActionResult> GenerateToken([FromBody] LoginDto model)
+    {
+        var user = await _userManager.FindByNameAsync(model.Username);
+        if (user == null || !await _userManager.CheckPasswordAsync(user, model.Password))
+            return Unauthorized("Invalid credentials");
 
-            var token = await _jwtService.GenerateToken(user);
+        var token = await _jwtService.GenerateToken(user);
 
-            return Ok(token);
-        }
+        return Ok(token);
     }
 }
--- TaskManagement.Domain/Models/BaseEntity.cs
+++ TaskManagement.Domain/Models/BaseEntity.cs
@@ -1,23 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TaskManagement.Domain.Exceptions;
 
-namespace TaskManagement.Domain.Models
+namespace TaskManagement.Domain.Models;
+
+public class BaseEntity
 {
-    public class BaseEntity
+    public BaseEntity()
     {
-        public int Id { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public bool Deleted { get; set; }
-        public BaseEntity()
-        {
-            CreatedAt = DateTime.Now;
-            UpdatedAt = DateTime.Now;
-            Id = 0;
-            Deleted = false;
-        }
+        CreatedAt = DateTime.Now;
+        UpdatedAt = DateTime.Now;
+        Id = 0;
+        Deleted = false;
+    }
+
+    public int Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    public bool Deleted { get; private set; }
+
+    public void Delete()
+    {
+        if (!Deleted) Deleted = true;
+        else throw new DomainException("Can not undelete an entity");
+    }
+
+    public void UpdateUpdatedAt()
+    {
+        UpdatedAt = DateTime.Now;
     }
 }
