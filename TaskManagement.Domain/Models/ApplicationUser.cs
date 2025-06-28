b728d8cc6efb720ab9bac8743767c934dbd6f9c2
Mehran Abdi <mehranabdi13793@gmail.com>
2025-06-28 17:16:34 +0330

    Change ApplicationUser.cs to DDD

--- TaskManagement.Domain/Models/ApplicationUser.cs
+++ TaskManagement.Domain/Models/ApplicationUser.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TaskManagement.Domain.Exceptions;
 
-namespace TaskManagement.Domain.Models
+namespace TaskManagement.Domain.Models;
+
+public class ApplicationUser : IdentityUser<int>
 {
-    public class ApplicationUser: IdentityUser<int>
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string PhoneNumber { get; set; }
+
+    public void SetFirstName(string firstName)
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PhoneNumber { get; set; }
+        if (string.IsNullOrWhiteSpace(firstName))
+            throw new DomainException("First name cannot be null or empty.");
+        FirstName = firstName;
+    }
+
+    public void SetLastName(string lastName)
+    {
+        if (string.IsNullOrWhiteSpace(lastName))
+            throw new DomainException("Last name cannot be null or empty.");
+        LastName = lastName;
     }
 }
