104288ba1c45549bedb652572a1516c81c4351a8
Mehran Abdi <mehranabdi13793@gmail.com>
2025-06-29 17:34:24 +0330

    Modify db configuration files to set delete action

--- TaskManagement.Repository/EntityConfigurations/TaskAssignmentRequestConfiguration.cs
+++ TaskManagement.Repository/EntityConfigurations/TaskAssignmentRequestConfiguration.cs
@@ -1,29 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using TaskManagement.Domain.Models;
 
-namespace TaskManagement.Repository.EntityConfigurations
+namespace TaskManagement.Repository.EntityConfigurations;
+
+public class TaskAssignmentRequestConfiguration : IEntityTypeConfiguration<TaskAssignmentRequest>
 {
-    public class TaskAssignmentRequestConfiguration : IEntityTypeConfiguration<TaskAssignmentRequest>
+    public void Configure(EntityTypeBuilder<TaskAssignmentRequest> builder)
     {
-        public void Configure(EntityTypeBuilder<TaskAssignmentRequest> builder)
-        {
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.IsAccepted).IsRequired();
-            builder.Property(x => x.RequestNotificationId);
-            builder.HasOne<ApplicationUser>()
-               .WithMany()
-               .HasForeignKey(x => x.AssigneeId)
-               .OnDelete(DeleteBehavior.NoAction);
-            builder.HasOne<ApplicationUser>()
-               .WithMany()
-               .HasForeignKey(x => x.TaskOwnerId)
-               .OnDelete(DeleteBehavior.NoAction);
-        }
+        builder.Property(x => x.IsAccepted).IsRequired();
+        builder.Property(x => x.RequestNotificationId);
+        builder
+            .HasOne(r => r.TaskOwner)
+            .WithMany()
+            .HasForeignKey(r => r.TaskOwnerId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
--- TaskManagement.Repository/EntityConfigurations/TaskCommentConfiguration.cs
+++ TaskManagement.Repository/EntityConfigurations/TaskCommentConfiguration.cs
@@ -1,25 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaskManagement.Domain.Models;
 
-namespace TaskManagement.Repository.EntityConfigurations
+namespace TaskManagement.Repository.EntityConfigurations;
+
+public class TaskCommentConfiguration : IEntityTypeConfiguration<TaskComment>
 {
-    public class TaskCommentConfiguration : IEntityTypeConfiguration<TaskComment>
+    public void Configure(EntityTypeBuilder<TaskComment> builder)
     {
-        public void Configure(EntityTypeBuilder<TaskComment> builder)
-        {
-            builder.HasKey(x => x.Id);
-            builder.Property(x => x.Text).IsRequired().HasColumnType("nvarchar(500)");
-            builder.Property(x => x.TaskId).IsRequired();
-            builder.Property(x => x.UserId).IsRequired();
-            builder.HasOne<AppTask>()
-               .WithMany()
-               .HasForeignKey(x => x.TaskId);
-        }
+        builder.Property(x => x.Text).IsRequired().HasColumnType("nvarchar(500)");
+        builder.Property(x => x.TaskId).IsRequired();
+        builder.Property(x => x.UserId).IsRequired();
+        builder
+            .HasOne(r => r.User)
+            .WithMany()
+            .HasForeignKey(r => r.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
