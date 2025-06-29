3541b967ae91ff44097c6f3136c9f7b9853828a1
Mehran Abdi <mehranabdi13793@gmail.com>
2025-06-29 17:33:18 +0330

    Refactor TaskAssignmentRequestRepository.cs

--- TaskManagement.Repository/Repositories/TaskAssignmentRequestRepository.cs
+++ TaskManagement.Repository/Repositories/TaskAssignmentRequestRepository.cs
@@ -1,31 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TaskManagement.Domain.Interfaces;
 using TaskManagement.Domain.Models;
 using TaskManagement.Shared.ServiceInterfaces;
 
-namespace TaskManagement.Repository.Repositories
+namespace TaskManagement.Repository.Repositories;
+
+public class TaskAssignmentRequestRepository : BaseRepository<TaskAssignmentRequest>, ITaskAssignmentRequestRepository
 {
-    public class TaskAssignmentRequestRepository : BaseRepository<TaskAssignmentRequest>, ITaskAssignmentRequestRepository
+    private readonly IUserContext userContext;
+
+    public TaskAssignmentRequestRepository(TaskManagementDBContext context, IUserContext userContext) : base(context)
     {
-        private readonly IUserContext userContext;
-        public TaskAssignmentRequestRepository(TaskManagementDBContext context, IUserContext userContext) : base(context)
-        {
-            this.userContext = userContext;
-        }
+        this.userContext = userContext;
+    }
 
-        public async Task<TaskAssignmentRequest> GetTaskAssignmentRequestByNotificationIdAsync(int notificationId)
-        {
-            return await _dbSet.FirstAsync(x => x.RequestNotificationId == notificationId);
-        }
+    public async Task<TaskAssignmentRequest> GetTaskAssignmentRequestByNotificationIdAsync(int notificationId)
+    {
+        var taskAssignmentRequest =
+            await _dbSet.FirstOrDefaultAsync(x => x.RequestNotificationId == notificationId && x.Deleted == false);
+        if (taskAssignmentRequest == null)
+            throw new InvalidOperationException(
+                $"TaskAssignmentRequest for notification with id {notificationId} not found");
+        return taskAssignmentRequest;
+    }
 
-        public async Task<bool> RequestAlreadyExists(int assigneeId, int taskId)
-        {
-            return await _dbSet.AnyAsync(x => x.AssigneeId == assigneeId && x.TaskId == taskId && x.TaskOwnerId == userContext.UserId && x.Deleted == false);
-        }
+    public async Task<bool> RequestAlreadyExists(int assigneeId, int taskId)
+    {
+        return await _dbSet.AnyAsync(x =>
+            x.AssigneeId == assigneeId && x.TaskId == taskId && x.TaskOwnerId == userContext.UserId &&
+            x.Deleted == false);
     }
 }
