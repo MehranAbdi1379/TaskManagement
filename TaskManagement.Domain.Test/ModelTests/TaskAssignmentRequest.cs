3f8b10d1ce67e0eefe6fa2dfaa101f92431aaf76
Mehran Abdi <mehranabdi13793@gmail.com>
2025-06-29 17:30:31 +0330

    add TaskAssignmentRequest test

---
+++ TaskManagement.Domain.Test/ModelTests/TaskAssignmentRequest.cs
@@ -0,0 +1,105 @@
+﻿using FluentAssertions;
+using TaskManagement.Domain.Models;
+
+public class TaskAssignmentRequestTests
+{
+    [Fact]
+    public void Constructor_WithValidParameters_ShouldInitializeProperties()
+    {
+        // Arrange
+        var taskOwnerId = 1;
+        var assigneeId = 2;
+        var taskId = 3;
+
+        // Act
+        var request = new TaskAssignmentRequest(taskOwnerId, assigneeId, taskId);
+
+        // Assert
+        request.TaskOwnerId.Should().Be(taskOwnerId);
+        request.AssigneeId.Should().Be(assigneeId);
+        request.TaskId.Should().Be(taskId);
+    }
+
+    [Fact]
+    public void SetRequestNotificationId_ShouldUpdateProperty()
+    {
+        var request = new TaskAssignmentRequest();
+
+        request.SetRequestNotificationId(99);
+
+        request.RequestNotificationId.Should().Be(99);
+    }
+
+    [Fact]
+    public void SetTaskOwnerId_ShouldUpdateProperty()
+    {
+        var request = new TaskAssignmentRequest();
+
+        request.SetTaskOwnerId(15);
+
+        request.TaskOwnerId.Should().Be(15);
+    }
+
+    [Fact]
+    public void SetAssigneeId_ShouldUpdateProperty()
+    {
+        var request = new TaskAssignmentRequest();
+
+        request.SetAssigneeId(25);
+
+        request.AssigneeId.Should().Be(25);
+    }
+
+    [Fact]
+    public void SetTaskId_ShouldUpdateProperty()
+    {
+        var request = new TaskAssignmentRequest();
+
+        request.SetTaskId(35);
+
+        request.TaskId.Should().Be(35);
+    }
+
+    [Fact]
+    public void SetIsAccepted_ShouldUpdateProperty()
+    {
+        var request = new TaskAssignmentRequest();
+
+        request.SetIsAccepted(true);
+        request.IsAccepted.Should().BeTrue();
+
+        request.SetIsAccepted(false);
+        request.IsAccepted.Should().BeFalse();
+    }
+
+    [Fact]
+    public void InheritsFromBaseEntity_ShouldHaveBaseProperties()
+    {
+        var request = new TaskAssignmentRequest();
+
+        request.CreatedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
+        request.UpdatedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
+        request.Id.Should().Be(0);
+        request.Deleted.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Relationships()
+    {
+        var request = new TaskAssignmentRequest();
+        var taskOwner = new ApplicationUser();
+        var assignee = new ApplicationUser();
+        var task = new AppTask();
+        var requestNotification = new BaseNotification();
+
+        request.Assignee = assignee;
+        request.TaskOwner = taskOwner;
+        request.Task = task;
+        request.RequestNotification = requestNotification;
+
+        request.Assignee.Should().Be(assignee);
+        request.TaskOwner.Should().Be(taskOwner);
+        request.Task.Should().Be(task);
+        request.RequestNotification.Should().Be(requestNotification);
+    }
+}
